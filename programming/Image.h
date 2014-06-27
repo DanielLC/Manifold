26bdf1def03c3743100df831901bfea8538327a8
DanielLC <daniel_l_c@yahoo.com>
2014-06-27 11:18:13 -0700

    I made it do just the wormhole again, to replace the buggy version in the slideshow. Also, it isn't as small, so it shows off how warped space is more.

--- programming/Image.h
+++ programming/Image.h
@@ -1,7 +1,11 @@
-#include <Eigen/Dense>
-using namespace Eigen;
+#ifndef image_h
+#define image_h
 
-class Image{
-	public:
-		Vector3d getVector();
-}
+#include <tr1/array>
+#include "Color.h"
+#include "Constants.h"
+
+typedef std::tr1::array<std::tr1::array<Color,WIDTH>,HEIGHT> Image;
+typedef std::tr1::shared_ptr<Image> ImagePtr;
+
+#endif
